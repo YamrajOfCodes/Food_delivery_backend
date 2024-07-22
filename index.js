47943c6ce17fd811eb19483dae0a86f78634ecd1
YamrajOfcodes <kundanpatil0111@.gmail.com>
2024-07-22 15:47:38 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const cors = require("cors")
+const dbconnect = require("./Db/connection");
+require('dotenv').config();
+
+
+app.use(cors());
+
+
+app.use(express.urlencoded()); 
+app.use(express.json());
+dbconnect();
+
+
+// admin Routes
+const adminRouter = require("./Routes/admin/adminRoutes");
+app.use("/admin/api",adminRouter);
+
+
+// userRoutes
+
+const userRouter = require("./Routes/user/userRoutes");
+app.use("/user/api",userRouter);
+
+
+// product Routes
+const productRouter = require("./Routes/product/productRoutes");
+app.use("/product/api",productRouter);
+
+
+
+
+app.get("/",(req,res)=>{
+ res.send("server is start")
+})
+
+app.listen(4000,()=>{
+    console.log("server is listening");
+})
